cfe5dad870f2018fdf3bb02e228746807db4a487
Insane- <dhvanil_patel@live.com>
2014-12-08 23:05:37 -0500

    Update Assignment8Java.js

--- Assignment8Java.js
+++ Assignment8Java.js
@@ -1,41 +1,37 @@
-/*Dhvanil Patel
-Dhvanil_patel@live.com
-UMass Lowell in course 91.461 GUI Programming I
-Started: 10/21/2014, last updated: 12/04/2014
-file-name: assignment6Java.js
-I got help from Mihir and Tak and I learned from them.
-Looking forward to learn everything in winter vacation with learnable.
-
-*/
-
+/*
+     91.461 Assignment 8: Using the jQuery Validation Plugin and jQueryUI Tabbed Widget
+     Mihir Patel, UMass Lowell Computer Science, mihir_patel@student.uml.edu
+     Created on November 24, 2014
+     I have received helpe from Takyiu Lo.
+     This webpage will get a range of multiplication table and creates it to new tabs. */
 $(document).ready(function() {
     var tabs = $("#tabs").tabs();
     /* Validator */
     $.validator.addMethod('GreaterThanStartRow', function(value, element, param) {
-        if (numTwo.value === "") {
+        if (pTwo.value === "") {
             return true;
         }
-        return parseInt(numTwo.value) >= parseInt(numOne.value);
-    }, "The Maximum Column Value must be greater than the Minimum Column Value.");
+        return parseInt(pTwo.value) >= parseInt(pOne.value);
+    }, "The Last row must be greater than the First row.");
     $.validator.addMethod('GreaterThanStartColumn', function(value, element, param) {
-        return parseInt(numFour.value) >= parseInt(numThree.value);
-    }, "The Maximum Row Value must be greater than the Minimum Row value.");
+        return parseInt(pFour.value) >= parseInt(pThree.value);
+    }, "The Last column must be greater than the Start column.");
     $('#form').validate({
         rules: {
-            numOne: {
+            pOne: {
                 required: true,
                 digits: true
             },
-            numTwo: {
+            pTwo: {
                 required: true,
                 digits: true,
                 GreaterThanStartRow: true
             },
-            numThree: {
+            pThree: {
                 required: true,
                 digits: true
             },
-            numFour: {
+            pFour: {
                 required: true,
                 digits: true,
                 GreaterThanStartColumn: true
@@ -51,43 +47,44 @@ $(document).ready(function() {
         /* "The validation plugin allows you to configure these class names"
          * http://stackoverflow.com/questions/6168926/jquery-validation-how-to-make-fields-red
          */
-       
+        errorClass: "my-error-class",
+        validClass: "my-valid-class"
     });
 
     function crTable(nextTabNo) {
         /* getting the four values from the user */
         /* putting a "+" to treat the value as a number instead of string */
-        var numOne = +document.getElementById("numOne").value;
-        var numTwo = +document.getElementById("numTwo").value;
-        var numThree = +document.getElementById("numThree").value;
-        var numFour = +document.getElementById("numFour").value;
+        var pOne = +document.getElementById("pOne").value;
+        var pTwo = +document.getElementById("pTwo").value;
+        var pThree = +document.getElementById("pThree").value;
+        var pFour = +document.getElementById("pFour").value;
         /* get the reference for the preview */
         var preview = document.getElementById(nextTabNo);
         /* creates a <table> element and a <tbody> element */
         var tbl = document.createElement("table");
         var tblBody = document.createElement("tbody");
         /* creating all cells */
-        for (var i = numOne, ii = numTwo + 1; i <= ii; ++i) {
+        for (var i = pOne, ii = pTwo + 1; i <= ii; ++i) {
             /* creates a table row */
             var row = document.createElement("tr");
-            for (var j = numThree, jj = numFour + 1; j <= jj; ++j) {
+            for (var j = pThree, jj = pFour + 1; j <= jj; ++j) {
                 /* creates a cell */
                 var cell = document.createElement("td");
                 var cellText;
                 /* give some style to the cell/table */
-                var cellStyle = "padding: 10px; color: black;";
-                if (i == numOne && j == numThree) {
+                var cellStyle = "padding: 10px; color: #FFF;";
+                if (i == pOne && j == pThree) {
                     cellText = document.createTextNode("");
-                    cell.setAttribute("style", cellStyle + "background-color: white");
-                } else if (i == numOne) {
+                    cell.setAttribute("style", cellStyle + "background-color: #FFF");
+                } else if (i == pOne) {
                     cellText = document.createTextNode(j - 1);
-                    cell.setAttribute("style", cellStyle + "background-color: purple");
-                } else if (j == numThree) {
+                    cell.setAttribute("style", cellStyle + "background-color: #000");
+                } else if (j == pThree) {
                     cellText = document.createTextNode(i - 1);
-                    cell.setAttribute("style", cellStyle + "background-color: purple");
+                    cell.setAttribute("style", cellStyle + "background-color: #000");
                 } else {
                     cellText = document.createTextNode((i - 1) * (j - 1));
-                    cell.setAttribute("style", cellStyle + "background-color: yellow");
+                    cell.setAttribute("style", cellStyle + "background-color: #FF0000");
                 }
                 /* add the text to cell */
                 cell.appendChild(cellText);
@@ -115,7 +112,7 @@ $(document).ready(function() {
         /* create a new tab with close button next to it
          * http://stackoverflow.com/questions/14357614/add-close-button-to-jquery-ui-tabs
          */
-        tabslist.append('<li id="li' + nextTabNo + '"><a href="#tab' + nextTabNo + '">' + 'Tab ' + (nextTabNo + 1) + +
+        tabslist.append('<li id="li' + nextTabNo + '"><a href="#tab' + nextTabNo + '">' + 'Tab ' + (nextTabNo + 1) + '<\/a><input name="check" type="checkbox" id="checkbox' +
             nextTabNo + '"><span id="tabspan' + nextTabNo + '" class="ui-icon ui-icon-circle-close"></span><\/li>');
         // add content to the new tab */
         tabsdiv.append('<div id="tab' + nextTabNo + '"><\/div>');
@@ -129,7 +126,8 @@ $(document).ready(function() {
         var panelId = $(this).closest("li").remove().attr("aria-controls");
         $("#" + panelId).remove();
         tabs.tabs("refresh");
-		$('#delete').click(function() {
+    });
+    $('#delete').click(function() {
         /* push id in the selected */
         var selected = [];
         $('input:checkbox:checked').each(function() {
@@ -144,5 +142,4 @@ $(document).ready(function() {
         }
         $('#tabs').tabs("refresh");
     });
-    
 });
